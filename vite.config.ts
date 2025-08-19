281a6d5fd06e67d71b3f6ed41acab3484168d99c
thiagovalencio1 <thiagoventuravalencio@gmail.com>
2025-08-19 16:44:48 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Adicione esta linha para o deploy no GitHub Pages
+  base: '/thIAguinhoSolucoes/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
