6958d325052342416682190757de53fc4d453a6f
thiagovalencio1 <thiagoventuravalencio@gmail.com>
2025-08-19 19:31:02 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   // Define o caminho base para o deploy no GitHub Pages.
-  // Esta é a única configuração de caminho necessária.
   base: '/thIAguinhoSolucoes/', 
   plugins: [react()],
 });
